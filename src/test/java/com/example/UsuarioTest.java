6c83b9970117e3c1b8f4888899ad13ea4040721d
monjimda <d.monco@alumnos.upm.es>
2015-11-10 12:56:02 +0100

    pruebas cucumber

--- src/test/java/com/example/UsuarioTest.java
+++ src/test/java/com/example/UsuarioTest.java
@@ -1,7 +1,5 @@
 package com.example;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
@@ -14,100 +12,90 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.example.controllers.ReferenciaController;
 import com.example.controllers.UsuarioController;
 import com.example.dao.UsuarioDAO;
-import com.example.models.Referencia;
 import com.example.models.Usuario;
-import com.example.utils.InitDB;
+
 
 public class UsuarioTest {
 	
-	private static final Logger log = LoggerFactory.getLogger(ResourceControllerTest.class);
+	private static final Logger log = LoggerFactory.getLogger(UsuarioTest.class);
 	private static UsuarioController controller;
 	private static UsuarioDAO dao;
-	private static Usuario usuario;
+	//private static Usuario usuario;
 
 	@Before
 	public void init() throws Exception {
 		
-//		dao = UsuarioDAO.getInstance();
-//		dao.clearStore();
-//		log.info("Inicio test");
+		//se ejecuta antes de cada @test
 		
 	}
 
 	@AfterClass
 	public static void end() throws Exception {
-//		controller.dropReferencia();
-//		InitDB.loadResources();
-//		log.info("Storage restored");
-//		log.info("End test");
+		
+		//se ejecuta al final de todos los test
+
 	}
 
 	@BeforeClass
 	public  static void initTest() throws Exception {
 		
+		//se ejecuta antes de todos los test
 		dao = UsuarioDAO.getInstance();
+		controller = UsuarioController.getInstance();
 		dao.clearStore();
 		log.info("Inicio de los test");
 	}
 
 	@Test
-	public void datosVaciosGet() {
-		log.info("Prueba inserccion de usuarios con base de datos vacia");
+	public void pruebaAltaDatos() {
+		
+		log.info("Prueba de alta de datos de usuarios:");
 		try {
-			
 			
 			List<Usuario> list = new ArrayList<Usuario>();
 			list = controller.getUsuarios();
 			if(!list.isEmpty()){
-				fail("la BDD no esta vacia");
+				throw new Exception("la base de datos no esta vacia");
+			}else{
+				log.info("Inicio de la base de datos correcta");				
 			}
-			
 		} catch (Exception e) {
-			fail("Error: " + e.getMessage());
+			fail("Error de inicio de base de datos: " + e.getMessage());
 		}
-	}
-	
-	@Test
-	public void datosSet() {
 		
-		log.info("Prueba inserccion de usuario con base de datos vacia");
 		try {
-			usuario = new Usuario("pepito","ROLE_ADMIN");
-			controller.createUsuario(usuario.getName(), usuario.getRole());
 			
-			usuario = controller.getUsuario(usuario.getName());
-			if(usuario == null){
-				fail("El usuario no se ha introducido");
+			List<Usuario> list = new ArrayList<Usuario>();
+			controller = UsuarioController.getInstance();
+			controller.createUsuario("rbrito", "validador");
+			list = controller.getUsuarios();
+			if(list.size() != 1){
+				throw new Exception("elementos distintos de los introducidos por defecto o exceso");
+			}else{
+				log.info("Insercion correcta");				
 			}
 			
 		} catch (Exception e) {
-			fail("Error: " + e.getMessage());
+			fail("Error de insercion: " + e.getMessage());
 		}
-		
+		try {
+			
+			List<Usuario> list = new ArrayList<Usuario>();
+			controller = UsuarioController.getInstance();
+			controller.createUsuario("msroa", "mantenimiento");
+			controller.createUsuario("dmonco","administrador");
+			controller.createUsuario("ogquevedo","consultor");
+			list = controller.getUsuarios();
+			if(list.size() != 4){
+				throw new Exception("elementos distintos de los introducidos por defecto o exceso");
+			}else{
+				log.info("Insercion multiple correcta");				
+			}
+			
+		} catch (Exception e) {
+			fail("Error de insercion: " + e.getMessage());
+		}
 	}
-
-//	@Test
-//	public void createResource() {
-////		log.info("Start 'Create Resource test'");
-////		try {
-////			Referencia r = new Referencia(0,"rbrito","GFI Centro","Banco","Desarrollo",null,10,"Denominacion","resumenProyecto","problematicaCliente","solucionGfi","Java",10,"http://imagen.jpg","Si",new int[]{10,11,12},"josem","pepes","rbrito","CodigoQR","Borrador");
-////			controller.createReferencia(r);
-////			r = controller.getReferencia(1);
-////			assertEquals(r.getCliente(), "rbrito");
-////		} catch (Exception e) {
-////			fail("Error: " + e.getMessage());
-////		}
-//	}
-//
-//	@Test(expected = Exception.class)
-//	public void createDubplicatedResource() throws Exception {
-////		log.info("Start 'Create Duplicated Resource test'");
-////		Referencia r = new Referencia(0,"rbrito","GFI Centro","Banco","Desarrollo",null,10,"Denominacion","resumenProyecto","problematicaCliente","solucionGfi","Java",10,"http://imagen.jpg","Si",new int[]{10,11,12},"josem","pepes","rbrito","CodigoQR","Borrador");
-////		controller.createReferencia(r);
-////		controller.createReferencia(r);
-//	}
-
 }
